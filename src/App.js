1de420ee40b045058f8e8d68f0dd817d053ad309
Patrycja20 <madydapatrycja97@o2.pl>
2018-08-21 23:25:13 +0200

    Umieszczenie mapy na stronie głównej.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Title"><h1>What's the weather?</h1></div>
-        <img src={Clouds}  className="Image" width="100%" height="30%"/>
-        <table><tr><td>Temperature</td><td>Precipitation</td><td>City</td></tr>
-          <tr><td><Temperature/></td><td><Precipitation/></td><td><City/></td></tr></table>
+        <div className="Background" width="100%" height="30%"><img src={Clouds}  className="Image" width="100%" height="100%"/></div>
+        <div className="Package"><div className="Block"><City/></div><div className="Block"><Precipitation/></div><div className="Block"><Temperature/></div></div>
+
       </div>
     );
   }
--- src/City.js
+++ src/City.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 class City extends React.Component {
+
   render() {
-    return <div> City
+    return <div> City: <select id="city">
+      <option value="l">London</option>
+      <option value="w" selecte>Warsaw</option>
+      <option value="p">Paris</option>
+      <option value="b">Berlin</option>
+      <option value="m">Madrid</option>
+    </select> <br/><br/>
+      <iframe src={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d312779.9431310756!2d20.781016711291045!3d52.232606289062204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc669a869f01%3A0x72f0be2a88ead3fc!2sWarszawa!5e0!3m2!1spl!2spl!4v1534881905344'} width="300" height="350"></iframe>
     </div>
   }
 }
